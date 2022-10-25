d3b1f1cb2844268a8dda63790aa8feadf05383d2
Vu Le <hoaivu100413@gmail.com>
2022-10-26 01:04:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! 55555');
+  res.send('Hello World! 666666');
 });
 
 var server = app.listen(3000, function () {
